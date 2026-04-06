8ccb56d4b7e3c85addbd1d008188fc0d6ae30ac1
notlemos <caiolemossfc123@hotmail.com>
2026-04-06 18:34:44 -0300

    Implementando casos de uso - TDD & Mocking

--- src/services/check-in.ts
+++ src/services/check-in.ts
@@ -16,6 +16,15 @@ import { CheckInsRepository } from "@/repositories/check-ins-repository";
     
         async execute({userId, gymId}: CheckInServiceRequest): Promise<CheckInServiceResponse> {
             
+            const checkInOnSameDay = await this.checkInsRepository.findByUserIdOnDate(
+                userId,
+                new Date()
+            )
+
+            if (checkInOnSameDay) {
+                throw new Error()
+            }
+
             const checkIn = await this.checkInsRepository.create({
                 gym_id: gymId,
                 user_id: userId
