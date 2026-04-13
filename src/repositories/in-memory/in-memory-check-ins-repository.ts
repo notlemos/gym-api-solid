47ee33b89e310b1601427f12036348118d693bc4
notlemos <caiolemossfc123@hotmail.com>
2026-04-13 17:41:39 -0300

    Implementando casos de uso - Caso de uso de validar de check-in

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -4,8 +4,17 @@ import { randomUUID } from "node:crypto";
 import dayjs = require("dayjs");
 
 export class InMemoryCheckInsRepository  implements CheckInsRepository {
+    
    
     public items: CheckIn[] = []
+
+    async findById(id: string) {
+        const checkIn = this.items.find((item) => item.id === id)
+        if (!checkIn ) {
+            return null 
+        }
+        return checkIn
+    }
 
     async findByUserIdOnDate(userId: string, date: Date){
         const startOfTheDay = dayjs(date).startOf('date')
@@ -33,6 +42,8 @@ export class InMemoryCheckInsRepository  implements CheckInsRepository {
     async countByUserId(userId: string) {
         return this.items.filter((item) => item.user_id === userId).length
     }
+    
+
     async create(data: Prisma.CheckInUncheckedCreateInput) {
         const checkIn = {
             id: randomUUID(),
@@ -42,6 +53,15 @@ export class InMemoryCheckInsRepository  implements CheckInsRepository {
             created_at: new Date()
         }
         this.items.push(checkIn)
+
+        return checkIn
+    }
+    async save(checkIn: CheckIn){
+        const checkInIndex = this.items.findIndex(items => items.id === checkIn.id)
+
+        if (checkInIndex >= 0 ) {
+            this.items[checkInIndex] = checkIn
+        }
 
         return checkIn
     }
