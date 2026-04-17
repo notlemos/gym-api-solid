69f529a1d4e4e8523c256ca58cc7a07cd72bc33d
notlemos <caiolemossfc123@hotmail.com>
2026-04-17 16:26:06 -0300

    Controllers & Testes E2E - Controller de perfil

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,8 +1,20 @@
+import { makeGetUserProfileService } from '@/services/factories/make-get-user-profile-service'
 import { FastifyRequest, FastifyReply } from 'fastify'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
     await request.jwtVerify()
 
-    console.log(request.user.sub)
-    return reply.status(200).send()
+    const getUserProfile = makeGetUserProfileService()
+
+    const { user } = await getUserProfile.execute({
+        userId: request.user.sub
+    })
+
+    
+    return reply.status(200).send({
+        user: {
+            ...user,
+            password_hash: undefined
+        }
+    })
 }
