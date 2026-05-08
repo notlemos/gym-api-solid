af57f7d42967ee18e1b49049d3e417ca5b5dd027
notlemos <caiolemossfc123@hotmail.com>
2026-05-08 16:54:17 -0300

    Refresh Token & RBAC - Autorização por cargos (RBAC)

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -3,6 +3,7 @@ import { verifyJWT } from "../../middlewares/verift-jwt";
 import { search } from "./search";
 import { nearby } from "./nearby";
 import { create } from "./create";
+import { verifyUserRole } from "@/http/middlewares/verify-user-role";
 
 
 export async function gymsRoutes(app: FastifyInstance) {
@@ -11,6 +12,6 @@ export async function gymsRoutes(app: FastifyInstance) {
     app.get('/gyms/search', search)
     app.get('/gyms/nearby', nearby)
 
-    app.post('/gyms', create)
+    app.post('/gyms', { onRequest: [verifyUserRole('ADMIN')] }, create)
 
 }
